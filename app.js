bac01a742ea06fbdf532d34cf910b40cca1fae4c
palaogluserdest-dev <palaogluserdest@hotmail.com>
2024-06-18 21:07:46 +0300

    Added passing effect for mobil menu and fixed responsive desing bugs

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const mobilMenu = document.querySelector("#hero-mobil-menu");
 const hamburgerIcon = document.querySelector("#hero-hamburger-icon");
 const closeIcon = document.querySelector("#hero-close-icon");
 
+// Mobil menu visibilty switcher function start
 const mobilMenuHandler = () => {
-  mobilMenu.classList.contains("active")
-    ? mobilMenu.classList.remove("active")
-    : mobilMenu.classList.add("active");
+  if (mobilMenu.classList.contains("active")) {
+    mobilMenu.classList.remove("active");
+    setTimeout(() => {
+      mobilMenu.style.visibilty = "hidden";
+      mobilMenu.style.opacity = "0";
+    }, 300);
+  } else {
+    mobilMenu.classList.add("active");
+    mobilMenu.style.visibilty = "visible";
+    mobilMenu.style.opacity = "1";
+  }
 
   mobilMenu.classList.contains("active")
     ? (closeIcon.style.display = "block")
@@ -16,3 +25,4 @@ const mobilMenuHandler = () => {
     ? (hamburgerIcon.style.display = "none")
     : (hamburgerIcon.style.display = "block");
 };
+// Mobil menu visibilty changer function start
