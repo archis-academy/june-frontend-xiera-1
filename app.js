6ca36103c6bbc9c5c439ecbdc5219b7709c6414d
parvizverdiyev <parviz.verdiyev@outlook.com>
2024-07-01 00:25:36 +0400

    [JU-6] Featured Jobs Section - fixed modul design

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const vacancies = [
   {
+    id: 1,
     JobTitle: "Senior UX/UI Developer with strong Designer skills",
     CompanyName: "Gucci",
     PostedDate: "5 days ago",
@@ -7,6 +8,7 @@ const vacancies = [
     JobType: "Part-Time",
   },
   {
+    id: 2,
     JobTitle: "Experienced Frontend Developer with a Focus on Accessibility",
     CompanyName: "Apple",
     PostedDate: "2 days ago",
@@ -14,6 +16,7 @@ const vacancies = [
     JobType: "Full-Time",
   },
   {
+    id: 3,
     JobTitle: "Backend Developer Specializing in Microservices Architecture",
     CompanyName: "Microsoft",
     PostedDate: "1 week ago",
@@ -21,6 +24,7 @@ const vacancies = [
     JobType: "Contract",
   },
   {
+    id: 4,
     JobTitle: "Full Stack Developer with Expertise in Cloud Technologies",
     CompanyName: "Google",
     PostedDate: "3 days ago",
@@ -28,6 +32,7 @@ const vacancies = [
     JobType: "Remote",
   },
   {
+    id: 5,
     JobTitle: "Product Designer with a Strong Background in User Research",
     CompanyName: "Facebook",
     PostedDate: "4 days ago",
@@ -35,6 +40,7 @@ const vacancies = [
     JobType: "Internship",
   },
   {
+    id: 6,
     JobTitle: "UX Researcher Focused on Improving User Engagement",
     CompanyName: "Amazon",
     PostedDate: "5 days ago",
@@ -42,6 +48,7 @@ const vacancies = [
     JobType: "Part-Time",
   },
   {
+    id: 7,
     JobTitle: "Mobile App Developer Specializing in iOS and Android Platforms",
     CompanyName: "Spotify",
     PostedDate: "1 day ago",
@@ -49,6 +56,7 @@ const vacancies = [
     JobType: "Full-Time",
   },
   {
+    id: 8,
     JobTitle: "Data Scientist with Expertise in Machine Learning Algorithms",
     CompanyName: "Netflix",
     PostedDate: "1 week ago",
@@ -56,6 +64,7 @@ const vacancies = [
     JobType: "Full-Time",
   },
   {
+    id: 9,
     JobTitle: "DevOps Engineer with Experience in Continuous Deployment",
     CompanyName: "Adobe",
     PostedDate: "3 days ago",
@@ -63,6 +72,7 @@ const vacancies = [
     JobType: "Contract",
   },
   {
+    id: 10,
     JobTitle: "Security Analyst Specializing in Threat Detection and Response",
     CompanyName: "IBM",
     PostedDate: "5 days ago",
@@ -70,6 +80,7 @@ const vacancies = [
     JobType: "Full-Time",
   },
   {
+    id: 11,
     JobTitle: "Software Engineer with Strong Problem-Solving Skills",
     CompanyName: "Intel",
     PostedDate: "2 days ago",
@@ -77,6 +88,7 @@ const vacancies = [
     JobType: "Remote",
   },
   {
+    id: 12,
     JobTitle: "UI Designer with a Passion for Creating Intuitive Interfaces",
     CompanyName: "Oracle",
     PostedDate: "4 days ago",
@@ -85,72 +97,86 @@ const vacancies = [
   },
 ];
 
+// Vacansies Cards Start
+
 let jobsVacancies = document.querySelector(".jobs-vacancies");
-let rightArrow = document.querySelector("#right-arrow");
-let leftArrow = document.querySelector("#left-arrow");
 
-let currentIndex = 0;
-const itemsPerPage = 6;
-
-function loadVacancies(startIndex, endIndex) {
-  jobsVacancies.innerHTML = "";
-  for (let i = startIndex; i < endIndex && i < vacancies.length; i++) {
-    jobsVacancies.innerHTML += `
-      <div class="vacancies-card" onclick="openPopup()">
-        <div class="vc-header">
-          <div class="vc-header-content">
-            <img
-              src="./images/featured-jobs/company-logo/nike.svg"
-              alt="${vacancies[i].CompanyName} Logo"
-            />
-            <div class="vc-company"> <h3>${vacancies[i].CompanyName}</h3>
-            <p>${vacancies[i].PostedDate}</p></div>
-          </div>
-          <div class="popup-close">
-            <button class="popup-close-btn" onclick="closePopup()">X</button>
-          </div>
-        </div>
-        <div class="vc-content">
-          <p>${vacancies[i].JobTitle}</p>
-        </div>
-        <div class="vc-info">
-          <span>${vacancies[i].Location}</span>
-          <span>${vacancies[i].JobType}</span>
-        </div>
-      </div>
-    `;
-  }
-}
-
-loadVacancies(0, itemsPerPage);
-
-let vacanciesCard = document.querySelector(".vacancies-card");
-let popupCloseBtn = document.querySelector(".popup-close-btn");
-
-rightArrow.addEventListener("click", () => {
-  currentIndex += itemsPerPage;
-  if (currentIndex >= vacancies.length) {
-    currentIndex = 0; //
-  }
-  loadVacancies(currentIndex, currentIndex + itemsPerPage);
+vacancies.map((vacancy) => {
+  jobsVacancies.innerHTML += `
+       <div class="vacancies-card">
+         <div class="vc-header">
+           <div class="vc-header-content">
+             <img
+               src="./images/featured-jobs/company-logo/nike.svg"
+               alt="${vacancy[" "]} Logo"
+             />
+             <div class="vc-company"> <h3>${vacancy["CompanyName"]}</h3>
+             <p>${vacancy["PostedDate"]}</p></div>
+           </div>
+           <div class="popup-close">
+             <button class="popup-close-btn">X</button>
+           </div>
+         </div>
+         <div class="vc-content">
+           <p>${vacancy["JobTitle"]}</p>
+         </div>
+         <div class="vc-info">
+           <span>${vacancy["Location"]}</span>
+           <span>${vacancy["JobType"]}</span>
+         </div>
+       </div>
+     `;
 });
 
-leftArrow.addEventListener("click", () => {
-  currentIndex -= itemsPerPage;
-  if (currentIndex < 0) {
-    currentIndex = Math.max(0, vacancies.length - itemsPerPage);
-  }
-  loadVacancies(currentIndex, currentIndex + itemsPerPage);
+// Vacansies Cards End
+
+const cards = document.querySelectorAll(".vacancies-card");
+const vacanciesModal = document.querySelector("#jobs-vacancies-modal");
+
+cards.forEach((card, index) => {
+  card.addEventListener("click", (e) => {
+    vacanciesModal.innerHTML = `<div class="vacancies-card">
+         <div class="vc-header">
+           <div class="vc-header-content">
+             <img
+               src="./images/featured-jobs/company-logo/nike.svg"
+               alt="${vacancies[index]} Logo"
+             />
+             <div class="vc-company"> <h3>${vacancies[index]}</h3>
+             <p>${vacancies[index]}</p></div>
+           </div>
+           <div class="popup-close">
+             <button class="popup-close-btn">X</button>
+           </div>
+         </div>
+         <div class="vc-content">
+           <p>${vacancies[index]}</p>
+         </div>
+         <div class="vc-info">
+           <span>${vacancies[index]}</span>
+           <span>${vacancies[index]}</span>
+         </div>
+       </div>`;
+  });
 });
 
-function openPopup() {
-  vacanciesCard.classList.add("jobs-popup");
-  jobsVacancies.classList.add("featured-jobs-blur");
-  popupCloseBtn.style.display = "block";
-}
+// See All Button Start
 
-function closePopup() {
-  jobsVacancies.classList.remove("featured-jobs-blur");
-  vacanciesCard.classList.remove("jobs-popup");
-  popupCloseBtn.style.display = "none";
-}
+let seeAllBtn = document.querySelector("#jobs-see-all-btn");
+let currentVacancy = 6;
+
+seeAllBtn.onclick = () => {
+  let nextVacancies = [
+    ...document.querySelectorAll(".jobs-vacancies .vacancies-card"),
+  ];
+  for (var i = currentVacancy; i < currentVacancy + 6; i++) {
+    nextVacancies[i].style.display = "inline-block";
+  }
+  currentVacancy += 6;
+
+  if (currentVacancy >= nextVacancies.length) {
+    seeAllBtn.style.display = "none";
+  }
+};
+
+// See All Button End
